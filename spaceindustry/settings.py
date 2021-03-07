56dc94e04d7bb0732bebf12626fbd49a5c4e58f3
Francesco Campanini <fcampanini@gmail.com>
2021-03-07 10:12:18 +0000

    Update settings.py
    removed secret

--- spaceindustry/settings.py
+++ spaceindustry/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'souyb69uefj7_xjg+lbe#ql&4zm@dl)*3!_mn9^(fdjbpnk^rv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
