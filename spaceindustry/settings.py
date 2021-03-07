e58245d6b39f69115a7bc8fcbb1cdfcd4c76f93a
Ubuntu <ubuntu@ip-172-31-3-88.eu-west-2.compute.internal>
2021-03-07 11:40:02 +0000

    adapted settings.py so that it will take env variables for prod

--- spaceindustry/settings.py
+++ spaceindustry/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmui5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
