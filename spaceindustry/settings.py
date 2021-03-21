6ef8ada62a02480851bdceab15daedf35a1206c8
Francesco Campanini <fcampanini@gmail.com>
2021-03-21 08:30:09 +0000

    production changes settings.py

--- spaceindustry/settings.py
+++ spaceindustry/settings.py
@@ -24,7 +24,7 @@ import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmui5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
